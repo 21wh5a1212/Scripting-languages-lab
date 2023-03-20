2e98bc78a99cb95661b0cc6ebe34f50a2ced1a75
mounika <109097297+21wh5a1212@users.noreply.github.com>
2023-03-20 11:21:42 +0530

    Update path.rb

--- path.rb
+++ path.rb
@@ -1,14 +1,14 @@
 # file name
-fbname = File.basename "C:/home/it/21wh5a1210/path.rb"
+fbname = File.basename "C:/home/it/21wh5a1212/path.rb"
 puts "File name: "+fbname
 
 # basename
-bname = File.basename "C:/home/it/21wh5a1210/path.rb",".rb"
+bname = File.basename "C:/home/it/21wh5a1212/path.rb",".rb"
 puts "Base name: "+bname
 
 # file extention
-ffextn = File.extname  "C:/home/it/21wh5a1210/path.rb"  
+ffextn = File.extname  "C:/home/it/21wh5a1212/path.rb"  
 puts "Extention: "+ffextn
 # path name
-path_name= File.dirname  "C:/home/it/21wh5a1210/path.rb"
+path_name= File.dirname  "C:/home/it/21wh5a1212/path.rb"
 puts "Path name: "+path_name
